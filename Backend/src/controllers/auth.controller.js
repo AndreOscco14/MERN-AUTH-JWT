9be6af161d893da62bed54d803a11d6feb75b9b1
bigDre82 <andre.oscco.14@gmail.com>
2023-07-02 19:22:00 +0200

    Cambios nuevos JWT

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import {createAccessToken}  from '../libs/jwt.js'
 
 export const register = async (req, res) => { 
     const {email , password, username} = req.body
@@ -17,7 +18,9 @@ export const register = async (req, res) => {
         })
 
        const userSaved = await newUser.save()
-        res.json({ 
+      const token =  await createAccessToken({id: userSaved._id})
+       res.cookie('token',token)
+       res.json({ 
             id: userSaved._id,
             username: userSaved.username,
             email: userSaved.email,
--- Backend/src/db.js
+++ Backend/src/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
     try {
         await mongoose.connect("mongodb://localhost/mernauth");
-            console.log('>>> BBDD  is coneccted');
+            console.log('>>> MongoDB  is coneccted <<<');
     } catch (error) {
         console.log(error);
     }
