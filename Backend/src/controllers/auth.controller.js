3b3273c143c55ba0f48b63f7efa1d1690e383215
bigDre82 <andre.oscco.14@gmail.com>
2023-07-03 13:31:42 +0200

    Login y Logout Tokens

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -32,6 +32,38 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-   res.send("Loginnn")
+export const login = async (req, res) => { 
+    const {email , password} = req.body
+    try {
+        // Buscamos si existe un usuario con esos datos.
+       const userFound = await User.findOne({email})
+       if(!userFound) {
+        return res.status(400).json({ message: "User not found"})
+       }
+
+        // el Compare devolverá un TRUE O FALSE
+        const isMatch = await bcrypt.compare(password, userFound.password)  //Genera un String aleatorio
+        if(!isMatch){
+            return res.status(400).json({ message: "Incorrect password"});
+        }
+
+      const token =  await createAccessToken({id: userFound._id})
+       res.cookie('token',token)
+       res.json({ 
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createAt: userFound.createdAt,
+            updateAt: userFound.updatedAt
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+}
+
+export const logout = async (req,res) => {
+    res.cookie('token', " ", {
+        expires : new Date(0),
+    })
+    return res.sendStatus(200)
 }
