2aac993005a2b920db58c020c884537023a6769f
bigDre82 <andre.oscco.14@gmail.com>
2023-07-04 17:46:28 +0200

    Modelo y enrutamiento tasks

---
+++ Backend/src/controllers/tasks.controller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.model.js'
+
+async function getTasks (req, res ) {}
+
+async function getTask (req, res){}
+
+async function createTask (req,res){}
+
+async function updateTask (req,res){}
+
+async function deleteTask (req, res){}
+
+export {
+    getTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
---
+++ Backend/src/models/task.model.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const tasksSchema = new mongoose.Schema({
+    title:{ type: String , required: true},
+    description: {type: String, required: true},
+    date: {type: Date, default: Date.now}, 
+}, {
+    timestamps: true
+})
