2594406ae4a1fd904f522169bd2faf2f2906a8ee
bigDre82 <andre.oscco.14@gmail.com>
2023-07-05 19:07:11 +0200

    Registro Front y Back

--- Front-React-Shop/src/pages/RegisterPage.jsx
+++ Front-React-Shop/src/pages/RegisterPage.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { registerRequest } from '../api/auth';
 
+
+
 function RegisterPage() {
 
     const {register, handleSubmit} = useForm()
 
+    const onSubmit =   handleSubmit(async (values) => {
+        const res = await registerRequest(values)
+        console.log(res)
+        });
+
     return (
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-            <form onSubmit={ handleSubmit(async (values) => {
-                // console.log(values)
-            const res = await registerRequest(values)
-            console.log(res)
-            })}>
+            <form onSubmit={ onSubmit}>
 
                 <input type='text' {...register("username", {required: true})}
                     className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
