77bd5ba267926a03ec4377c95abbd9e88b7dbbf8
bigDre82 <andre.oscco.14@gmail.com>
2023-07-12 20:48:58 +0200

    New Pages y rutas protegidas init

--- Front-React-Shop/src/App.jsx
+++ Front-React-Shop/src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {AuthProvider} from './context/AuthContext'
+
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
-import {AuthProvider} from './context/AuthContext'
+import TasksPage from './pages/TasksPage'
+import TasksFormPage from './pages/TasksFormPage'
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
 <AuthProvider>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element= {<h1>Home Page</h1>} />
+        <Route path='/' element= {<HomePage/>} />
         <Route path='/login' element= {<LoginPage/>} />
         <Route path='/register' element= {<RegisterPage/>} />
-        <Route path='/tasks' element= {<h1>Tasks page</h1>} />
-        <Route path='/add-task' element= {<h1>New Task</h1>} />
-        <Route path='/tasks/:id' element= {<h1>Update Task</h1>} />
-        <Route path='/profile' element= {<h1>Profile</h1>} />
+        <Route path='/tasks' element= {<TasksPage/>} />
+        <Route path='/add-task' element= {<TasksFormPage/>} />
+        <Route path='/tasks/:id' element= {<TasksFormPage/>} />
+        <Route path='/profile' element= {<ProfilePage/>} />
       </Routes>
     </BrowserRouter>
 </AuthProvider>
