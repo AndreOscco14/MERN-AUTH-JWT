0f6d8c35fefd117b39b8e3ffeca1e21cd36d3945
bigDre82 <andre.oscco.14@gmail.com>
2023-07-12 20:15:38 +0200

    Validaciones y Login user

--- Front-React-Shop/src/pages/RegisterPage.jsx
+++ Front-React-Shop/src/pages/RegisterPage.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+
 
 
 function RegisterPage() {
 
-    const {register, handleSubmit,
-        formState: {
-             errors }} = useForm()
+    const {register, handleSubmit, formState: {errors }} = useForm()
 
     const {signup, user, isAuthenticated, errors: registerErrors} = useAuth();
     const navigate = useNavigate()
@@ -24,13 +24,11 @@ function RegisterPage() {
 
     return (
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-        {
-            registerErrors.map((error,i) => (
+        { registerErrors.map((error,i) => (
                 <div className='bg-red-500 p-2 text-white' key={i}>
                     {error}
                 </div>
-            ))
-        }
+        ))}
             <form onSubmit={ onSubmit}>
                 <input type='text' {...register("username", {required: true})}
                     className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
@@ -54,6 +52,9 @@ function RegisterPage() {
                     Register
                 </button>
             </form>
+            <p className='flex gap-x-2 justify-between'>
+              Already have an account? <Link to="/login" className='text-sky-500'>Login</Link>
+            </p>
         </div>
       );
     }
