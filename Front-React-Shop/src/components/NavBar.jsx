3c3f4dea91d70e52f608ad0b3309e2c34fdb3eb2
bigDre82 <andre.oscco.14@gmail.com>
2023-12-10 18:06:56 +0100

    [UPDATE] - FlowBite NavBar Tailwind

--- Front-React-Shop/src/components/NavBar.jsx
+++ Front-React-Shop/src/components/NavBar.jsx
@@ -2,18 +2,12 @@ import React, { useState } from "react";
 
 import { Link } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+
 
 function NavBar() {
 
-
   const { isAuthenticated, logout, user } = useAuth()
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  function toggleMenu(){
-    setIsMenuOpen(!isMenuOpen)
-  }
-
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 mb-3">
@@ -25,19 +19,8 @@ function NavBar() {
       </Link>
 
 
-      {/* <ul className="flex gap-x-2"> */}
-
-
-      {/* SI está autenticado */}
         {isAuthenticated ? ( 
           <>
-
-
-
-
-
-
-
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 
 
@@ -47,7 +30,6 @@ function NavBar() {
                       aria-expanded="true" 
                       data-dropdown-toggle="user-dropdown" 
                       data-dropdown-placement="bottom"
-                      onClick={() => setIsMenuOpen(!isMenuOpen)}
                       >
                 <span className="sr-only">Open user menu</span>
                 <img className="w-8 h-8 rounded-full" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user photo"/>
@@ -95,7 +77,6 @@ function NavBar() {
               </button>
             </div>
 
-
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
@@ -112,139 +93,12 @@ function NavBar() {
                   Tasks
                 </Link>
               </li>
-              {/* <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-              </li> */}
             </ul>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* 
-            <li>
-             Hi: {user.username} !
-            </li>
-            <li>
-              <Link to='/add-task'
-                className="bg-gray-600 px-4 py-1 rounded-lg"
-              >
-              New Task
-              </Link>
-            </li>
-            <li>
-              <Link to='/tasks'
-                className="bg-gray-600 px-4 py-1 rounded-lg"
-              >
-              Tasks</Link>
-            </li>
-            <li>
-              <Link to='/' onClick={() => {
-                logout()
-              }}
-              className="bg-gray-600 px-4 py-1 rounded-lg"
-              >
-                Log Out
-              </Link>
-            </li>
- */}
-
-
-
-          </>
+        </>
           ):(
           <>
-
-
-
-
-
-
-              {/* SI NO está autenticado */}
-            {/* <li>
-              <Link to='/login'
-               className="bg-gray-600 px-4 py-1 rounded-lg"
-              >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to='/register'
-                className="bg-gray-600 px-4 py-1 rounded-lg"
-              >
-              Register</Link>
-            </li> */}
-
-
-
-
-
-
-
-
             <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-
-
-
-              {/* <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                <span className="sr-only">Open user menu</span>
-                <img className="w-8 h-8 rounded-full" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user photo"/>
-              </button> */}
-
-
-
-              {/* <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-                <div className="px-4 py-3">
-                    <span className="block text-sm text-gray-900 dark:text-white"> {user.username}  </span>
-                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
-                </div>
-                <ul className="py-2" aria-labelledby="user-menu-button">
-                  <li>
-                    <Link to='/add-task'
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                    New Task
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/tasks'
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Tasks
-                    </Link>
-                  </li>
-                  <li>
-                  <Link to='/' onClick={() => {
-                      logout()
-                    }}
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Sign out
-                    </Link>
-                  </li>
-                </ul>
-              </div> */}
-
               <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
