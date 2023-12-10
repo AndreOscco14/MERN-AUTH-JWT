866be37ae8c66ecf4eb612130a11fffaf62fa254
bigDre82 <andre.oscco.14@gmail.com>
2023-12-10 17:00:12 +0100

    [NavBar State]

--- Front-React-Shop/src/components/NavBar.jsx
+++ Front-React-Shop/src/components/NavBar.jsx
@@ -1,15 +1,19 @@
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
-import React, { useState } from "react";
 
 function NavBar() {
 
 
   const { isAuthenticated, logout, user } = useAuth()
 
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
-    };
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu(){
+    setIsMenuOpen(!isMenuOpen)
+  }
+
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 mb-3">
@@ -40,16 +44,18 @@ function NavBar() {
               <button type="button" 
                       className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" 
                       id="user-menu-button" 
-                      aria-expanded="false" 
+                      aria-expanded="true" 
                       data-dropdown-toggle="user-dropdown" 
                       data-dropdown-placement="bottom"
-                      onClick={toggleMenu}
+                      onClick={() => setIsMenuOpen(!isMenuOpen)}
                       >
                 <span className="sr-only">Open user menu</span>
                 <img className="w-8 h-8 rounded-full" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user photo"/>
               </button>
 
-              <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+
+              <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" 
+                   id="user-dropdown">
                 <div className="px-4 py-3">
                     <span className="block text-sm text-gray-900 dark:text-white"> {user.username}  </span>
                     <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
@@ -80,7 +86,7 @@ function NavBar() {
                   </li>
                 </ul>
               </div>
-
+ 
               <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
