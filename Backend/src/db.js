409fed9c6c39faae5811c34124928aeb11d64db3
bigDre82 <andre.oscco.14@gmail.com>
2023-12-11 00:33:09 +0100

    [UPDATE] MongoDB Atlas

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 
 export const connectDB = async () => {
+    dotenv.config()
     try {
-        await mongoose.connect("mongodb://localhost/mernauth");
+        // Conectado a MongooAtlas
+        await mongoose.connect(process.env.MONGODB_URL);
+        
+        // Asi llamo a la BBDD local. mernauth
+        
+        // await mongoose.connect("mongodb://localhost/mernauth");
             console.log('>>> MongoDB  is coneccted <<<');
     } catch (error) {
         console.log(error);
