e9f1f167a915a5cb323170fb39c30cf04756859b
bigDre82 <andre.oscco.14@gmail.com>
2023-12-11 01:54:13 +0100

    Cambios

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,14 +4,18 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import authRouter from './routes/auth.routes.js'
 import tasksRouter from './routes/tasks.routes.js'
-import dotenv from 'dotenv'
 
 const app = express()
-dotenv.config()
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://dre-fronted.onrender.com'
+]
 
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin: process.env.FRONTED_URL,
+    // origin: process.env.FRONTED_URL,
+    origin: allowedOrigins,
     credentials: true
 }))
 app.use(morgan('dev'));
